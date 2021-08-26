cd74aad8b5eff05818a55676c255fe53342c70d9
Bui Phu Khuyen <khuyenpb@gmail.com>
2021-08-26 17:20:06 +0700

    Update Folder Project

--- FakeNewsFilter.API/Controllers/NewsController.cs
+++ FakeNewsFilter.API/Controllers/NewsController.cs
@@ -1,46 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FakeNewsFilter.Application.Catalog.NewsManage;
+using FakeNewsFilter.ViewModel.Catalog.NewsManage;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.Threading.Tasks;
 
 namespace FakeNewsFilter.API.Controllers
 {
     [Route("api/[controller]")]
-    public class NewsController : Controller
+    [ApiController]
+    [Authorize]
+    public class NewsController : ControllerBase
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly INewsService _newsService;
+
+        public NewsController(INewsService newsService)
         {
-            return new string[] { "value1", "value2" };
+            _newsService = newsService;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Create([FromForm] NewsCreateRequest request)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var newsId = await _newsService.Create(request);
+
+            if (newsId == 0)
+            {
+                return BadRequest("Cannot create news");
+            }
+
+            var news = await _newsService.GetById(newsId);
+
+            return CreatedAtAction(nameof(GetById), new { newsId = newsId }, news);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpDelete("{newsId}")]
+        public async Task<IActionResult> Delete(int newsId)
         {
+            var result = await _newsService.Delete(newsId);
+
+            if (result == 0)
+            {
+                return BadRequest("Cannot delete this news");
+            }
+            return Ok("Deleted successfully");
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET: api/news
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Get()
         {
+            var topics = await _newsService.GetAll();
+
+            return Ok(topics);
+        }
+
+        [HttpGet("{newsId}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetById(int newsId)
+        {
+            var news = await _newsService.GetById(newsId);
+
+            if (news == null)
+            {
+                return NotFound("Cannot find product");
+            }
+            return Ok(news);
+        }
+
+        // GET: api/news/topic
+        [HttpGet("topic")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetNewsInTopic([FromQuery] GetPublicNewsRequest request)
+        {
+            var newsintopics = await _newsService.GetNewsInTopic(request);
+
+            if (newsintopics.Count == 0)
+            {
+                return NotFound($"Cannot find product in topic {request.TopicId}");
+            }
+            return Ok(newsintopics);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromForm] NewsUpdateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _newsService.Update(request);
+
+            if (result == 0)
+            {
+                return BadRequest("Cannot update this news");
+            }
+            return Ok("Updated successfully");
+        }
+
+        [HttpPatch("link/{newsId}/{newLink}")]
+        public async Task<IActionResult> UpdateLink(int newsId, string newLink)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await _newsService.UpdateLink(newsId, newLink);
+
+            if (result == false)
+            {
+                return BadRequest("Cannot update link this news");
+            }
+            return Ok("Updated link successfully");
         }
     }
 }
--- FakeNewsFilter.API/Program.cs
+++ FakeNewsFilter.API/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace FakeNewsFilter.API
+namespace FakeNewsFilter
 {
     public class Program
     {
