d429998c7faa4296bf7b29533e16c36da35a9357
xuanthanh2609 <thanh26092000@gmail.com>
2022-07-22 21:10:38 +0700

    Create policy page

---
+++ FakeNewsFilter.UserApp/Controllers/PolicyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FakeNewsFilter.UserApp.Models;
+using System.Diagnostics;
+
+namespace FakeNewsFilter.UserApp.Controllers
+{
+    public class PolicyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/policy")]
+        public IActionResult Policy()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
