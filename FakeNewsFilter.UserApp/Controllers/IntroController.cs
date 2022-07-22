f0ed815edd369219ccf2e47195ed977ec00667e5
xuanthanh2609 <thanh26092000@gmail.com>
2022-07-22 17:51:40 +0700

    landing page (_layout error)

---
+++ FakeNewsFilter.UserApp/Controllers/IntroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FakeNewsFilter.UserApp.Models;
+using System.Diagnostics;
+
+
+namespace FakeNewsFilter.UserApp.Controllers
+{
+    public class IntroController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public IntroController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet("/intro")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
