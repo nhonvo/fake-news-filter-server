fd038ec9536f25a9f3b7b5ce88da461eea46622d
Bui Phu Khuyen <khuyenpb@gmail.com>
2022-08-04 11:34:21 +0700

    [UserApp] Fix Landing Page

---
+++ FakeNewsFilter.UserApp/LocalizationResources/ExpressLocalizationResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FakeNewsFilter.UserApp.LocalizationResources
+{
+    public class ExpressLocalizationResource
+    {
+        public ExpressLocalizationResource()
+        {
+        }
+    }
+}
+
---
+++ FakeNewsFilter.UserApp/LocalizationResources/ViewLocalizationResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FakeNewsFilter.UserApp.LocalizationResources
+{
+    public class ViewLocalizationResource
+    {
+        public ViewLocalizationResource()
+        {
+        }
+    }
+}
+
