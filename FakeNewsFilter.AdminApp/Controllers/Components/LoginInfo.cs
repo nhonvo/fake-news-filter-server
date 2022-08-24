a5798fb88b6ccfc7eb2137ffc1d62f6ded22115a
Khánh Huỳnh Hữu <khanh201011@gmail.com>
2022-08-24 21:07:53 +0700

    fix user manager

--- FakeNewsFilter.AdminApp/Controllers/Components/LoginInfo.cs
+++ FakeNewsFilter.AdminApp/Controllers/Components/LoginInfo.cs
@@ -11,7 +11,6 @@ namespace FakeNewsFilter.AdminApp.Controllers.Components
     {
         public Task<IViewComponentResult> InvokeAsync()
         {
-
             // Cast to ClaimsIdentity.
             var identity = HttpContext.User.Identity as ClaimsIdentity;
 
@@ -24,20 +23,20 @@ namespace FakeNewsFilter.AdminApp.Controllers.Components
                 .FirstOrDefault();
 
             var avatar = claim
-               .Where(x => x.Type == ClaimTypes.Uri)
-               .FirstOrDefault();
+                .Where(x => x.Type == ClaimTypes.Uri)
+                .FirstOrDefault();
 
             var role = claim
-               .Where(x => x.Type == ClaimTypes.Role)
-               .FirstOrDefault();
+                .Where(x => x.Type == ClaimTypes.Role)
+                .FirstOrDefault();
 
             var userinfo = new UserViewModel
             {
                 FullName = name.Value,
                 Avatar = avatar.Value,
-                Role = role.Value,
+                Roles = new List<string>() {role.Value}
             };
-            return Task.FromResult((IViewComponentResult)View("Default", userinfo));
+            return Task.FromResult((IViewComponentResult) View("Default", userinfo));
         }
     }
 }
