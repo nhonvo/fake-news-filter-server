b28bdbf6fa794b6827265222d48e677591f787ea
Khánh Huỳnh Hữu <khanh201011@gmail.com>
2022-10-08 19:10:05 +0700

    cron job clone news from oigetit

---
+++ FakeNewsFilter.API/Quartz/CloneNewsJob.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using FakeNewsFilter.Application.Catalog;
+using FakeNewsFilter.Utilities.Constants;
+using Quartz;
+
+
+namespace FakeNewsFilter.Quartz;
+
+public class CloneNewsJob : IJob
+
+{
+    private readonly INewsService _newsService;
+    private readonly ICloneNewsService _cloneNewsService;
+
+    public CloneNewsJob(INewsService newsService, ICloneNewsService cloneNewsService)
+    {
+        _newsService = newsService;
+        _cloneNewsService = cloneNewsService;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        foreach (var topic in SystemConstants.MapOigetitTopic)
+        {
+            var sysTopicId = SystemConstants.MapSysTopics[topic.Key];
+            
+            var news = await _cloneNewsService.GetOigetitCategory(topic.Value, sysTopicId);
+
+            await _newsService.CreateBatchNews(news.ResultObj, sysTopicId);
+        }
+    }
+}
--- FakeNewsFilter.Utilities/Constants/SystemConstants.cs
+++ FakeNewsFilter.Utilities/Constants/SystemConstants.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using System.Collections.Generic;
 namespace FakeNewsFilter.Utilities.Constants
 {
     public class SystemConstants
     {
         public const string MainConnectionString = "DefaultConnection";
+        
+        public static Dictionary<string, int> MapSysTopics = new Dictionary<string, int>()
+        {
+            {"Technology", 23},
+            {"Entertainment", 24},
+            {"Business", 25},
+            {"Lifestyle", 26},
+            {"Travel", 27}
+        };
 
+        public static Dictionary<string, int> MapOigetitTopic = new Dictionary<string, int>()
+        {
+            {"Technology", 3},
+            {"Entertainment", 7},
+            {"Business", 4},
+            {"Lifestyle", 5},
+            {"Travel", 6}
+        };
+        
         public class AppSettings
         {
             public const string DefaultLanguageId = "DefaultLanguageId";
